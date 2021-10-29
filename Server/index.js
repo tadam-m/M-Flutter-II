1971e0eafc25d05fc709a2c24d62a58da79e946d
guillaume <guillaume.lemariey@epitech.eu>
2021-10-29 18:54:56 +0200

    login and register api are done

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var ManageAccount = require('./routes/ManageAccount');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/gehenSiezumClub', ManageAccount);
+
+module.exports = app;
