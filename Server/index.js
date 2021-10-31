5414209f7629a32de9e8e79343e27ac9e389a013
Ersikan <julien.philippon@epitech.eu>
2021-10-31 11:24:29 +0100

    feat(Server): Add prettier and prettify everything

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var ManageAccount = require('./routes/ManageAccount');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var ManageAccount = require("./routes/ManageAccount");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/gehenSiezumClub', ManageAccount);
+app.use("/api/gehenSiezumClub", ManageAccount);
 
 module.exports = app;
--- Server/routes/ManageAccount.js
+++ Server/routes/ManageAccount.js
@@ -1,44 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var users = new Map();
-var Response = require('../bin/response.js').Response;
+var Response = require("../bin/response.js").Response;
 
-router.post('/login', function (req, res, next)
-{
-    let response = login(req);
-    res.status(response.status).json(response.body);
+router.post("/login", function (req, res, next) {
+  let response = login(req);
+  res.status(response.status).json(response.body);
 });
 
-router.post('/register', function (req, res, next)
-{
-    let response = register(req);
-    res.status(response.status).json(response.body);
+router.post("/register", function (req, res, next) {
+  let response = register(req);
+  res.status(response.status).json(response.body);
 });
 
-function register(req)
-{
-    var username = req.body.username;
-    var password = req.body.password;
+function register(req) {
+  var username = req.body.username;
+  var password = req.body.password;
 
-    if (username == undefined || password == undefined)
-        return new Response(403, "username or password is empty");
-    if (users.get(username) != undefined)
-        return new Response(401, "This user already exist");
-    else
-        users.set(username, password);
-    return new Response(200, "User " + username + " well created");
+  if (username == undefined || password == undefined)
+    return new Response(403, "username or password is empty");
+  if (users.get(username) != undefined)
+    return new Response(401, "This user already exist");
+  else users.set(username, password);
+  return new Response(200, "User " + username + " well created");
 }
 
-function login(req)
-{
-    var username = req.body.username;
-    var password = req.body.password;
+function login(req) {
+  var username = req.body.username;
+  var password = req.body.password;
 
-    if (username == undefined || password == undefined)
-        return new Response(403, "username or password is empty");
-    if (users.get(username) == password)
-        return new Response(200, "You are now connected");
-    else
-        return new Response(401, "The username and password doesn't match");
+  if (username == undefined || password == undefined)
+    return new Response(403, "username or password is empty");
+  if (users.get(username) == password)
+    return new Response(200, "You are now connected");
+  else return new Response(401, "The username and password doesn't match");
 }
 module.exports = router;
