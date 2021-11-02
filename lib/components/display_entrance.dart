e67fe0368daefcc322edba3e045b63a9c7b6d8d8
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-11-02 17:36:59 +0100

    Added widget to display ratings

---
+++ lib/components/display_entrance.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:techno_clubs_berlin/models/club.dart';
+
+class DisplayEntrance extends StatelessWidget {
+  final Entrance entrance;
+
+  const DisplayEntrance(this.entrance, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<Entrance, Icon> map = {
+      Entrance.easy: const Icon(Icons.shield_outlined),
+      Entrance.medium: const Icon(Icons.security),
+      Entrance.hard: const Icon(Icons.shield),
+    };
+
+    return map[entrance]!;
+  }
+}
+
+class DisplayEntranceString extends StatelessWidget {
+  final Entrance entrance;
+
+  const DisplayEntranceString(this.entrance, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(entrance.toString().split('.').last);
+  }
+}
