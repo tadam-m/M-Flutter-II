06527a4f40f6dcae5fc8cdec3715d137fd4110ca
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-11-02 18:01:52 +0100

    Removed image

--- lib/screens/auth/vue_club_screen.dart
+++ lib/screens/auth/vue_club_screen.dart
@@ -19,122 +19,109 @@ class VueClubScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              FittedBox(
-                child: Image.network(
-                  'https://images.xceed.me/clubs/covers/salon-zur-wilden-renate-club-berlin-xceed-5d87.jpg',
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  club.name,
+                  style: Theme.of(context).textTheme.headline1,
                 ),
-                fit: BoxFit.fill,
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      club.name,
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Row(
-                        children: [
-                          const Icon(Icons.place),
-                          Text(
-                            club.location,
-                            style: Theme.of(context).textTheme.bodyText1,
-                          ),
-                        ],
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.place),
+                      Text(
+                        club.location,
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
-                    ),
-                    const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  club.description,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
                     Text(
-                      club.description,
+                      'Camera',
                       style: Theme.of(context).textTheme.bodyText1,
                     ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Camera',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        const SizedBox(width: 10),
-                        Icon(
-                          club.camera
-                              ? Icons.photo_camera
-                              : Icons.no_photography,
-                        ),
-                        const SizedBox(width: 10),
-                        Text(
-                          club.camera ? "Allowed" : "Forbidden",
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                      ],
+                    const SizedBox(width: 10),
+                    Icon(
+                      club.camera ? Icons.photo_camera : Icons.no_photography,
                     ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Price',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        const SizedBox(width: 10),
-                        const Icon(Icons.euro),
-                        const SizedBox(width: 10),
-                        Text(
-                          club.price,
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Entrance',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        const SizedBox(width: 10),
-                        DisplayEntrance(club.entrance),
-                        const SizedBox(width: 10),
-                        DisplayEntranceString(club.entrance),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Best Clubbing Time',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        const SizedBox(width: 10),
-                        const Icon(Icons.schedule),
-                        const SizedBox(width: 10),
-                        Text(
-                          club.clubbingTime,
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          'Rating',
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                        const SizedBox(width: 10),
-                        DisplayRating(club.rating),
-                      ],
+                    const SizedBox(width: 10),
+                    Text(
+                      club.camera ? "Allowed" : "Forbidden",
+                      style: Theme.of(context).textTheme.bodyText1,
                     ),
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Price',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const SizedBox(width: 10),
+                    const Icon(Icons.euro),
+                    const SizedBox(width: 10),
+                    Text(
+                      club.price,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Entrance',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const SizedBox(width: 10),
+                    DisplayEntrance(club.entrance),
+                    const SizedBox(width: 10),
+                    DisplayEntranceString(club.entrance),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Best Clubbing Time',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const SizedBox(width: 10),
+                    const Icon(Icons.schedule),
+                    const SizedBox(width: 10),
+                    Text(
+                      club.clubbingTime,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      'Rating',
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    const SizedBox(width: 10),
+                    DisplayRating(club.rating),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
