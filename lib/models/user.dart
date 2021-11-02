c53d19888bf80aacd0a070a44024f02eedbfad4a
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-11-02 17:34:33 +0100

    Removed model parent

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,43 +1,9 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:typed_data';
-
-import 'model.dart';
-
-class User extends Model {
+class User {
   String name;
-  //String email;
   String password;
-  Uint8List? picture;
 
   User({
-    String? id,
     required this.name,
-    //required this.email,
     required this.password,
-    File? picture,
-  })  : picture = picture?.readAsBytesSync(),
-        super(id: id);
-
-  @override
-  User.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-      //  email = json['email'],
-        password = json['password'],
-        picture =
-        json['picture'] != null ? base64Decode(json['picture']) : null,
-        super.fromJson(json);
-
-  @override
-  Map<String, dynamic> toJson() => <String, dynamic>{
-    'name': name,
-   // 'email': email,
-    'password': password,
-    'picture': picture != null ? base64Encode(picture!) : null,
-    'id': id,
-  };
-
-  void updatePicture(File picture) {
-    this.picture = picture.readAsBytesSync();
-  }
+  });
 }
