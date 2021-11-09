9a393471f7e1ef3c17ece1f9c5218b455c7fff90
guillaume <guillaume.lemariey@epitech.eu>
2021-11-09 17:34:03 +0100

    [EDIT] edit review now you have many champs with name, mark, content etc

--- Server/routes/ManageReviews.js
+++ Server/routes/ManageReviews.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/PostComment', function (req, res, next)
-{
-    try {
-    let response = PostComment(req);
-    res.status(response.status).json(response.body);
-    } catch (error) {
-        return {status: 500, body: {message: {message : "Internal error"}}};
-    }
-});
-
 router.post('/PostReview', function (req, res, next)
 {
     try {
@@ -23,36 +13,22 @@ router.post('/PostReview', function (req, res, next)
 
 function PostReview(req)
 {
-    var username = req.body.username;
+    var username = req.body.reviewerName;
+    var reviewContent = req.body.reviewContent;
+    var mark = req.body.mark;
+    var date = req.body.date;
     var club = req.body.club;
-    var review = req.body.review;
 
-    if (review == undefined || Number(review) == 'NaN' || username == undefined || club == undefined)
+    if (mark == undefined || Number(mark) == 'NaN' || username == undefined || club == undefined)
         return {status: 403, body: {message: {message : "username, club or review is undefined"}}};
     var tmp = global.ClubList.get(club);
     if (tmp == undefined || global.UsersList.get(username) == undefined)
         return {status: 401, body: {message: {message :  "Bad request"}}};
     var rate = tmp.rate * tmp.reviews;
     tmp.reviews++;
-    tmp.rate = (rate + review) / tmp.reviews;
-    global.ClubList.set(club, tmp);
-    return {status: 200, body: tmp};
+    tmp.rate = (rate + mark) / tmp.reviews;
+    global.ClubList.get(club).commentaries.push({reviewerName: username, reviewContent: reviewContent, mark: mark, date : date});
+    return {status: 200, body: global.ClubList.get(club)};
 }
 
-function PostComment(req)
-{
-    var username = req.body.username;
-    var club = req.body.club;
-    var review = req.body.review;
-
-    if (username == undefined || club == undefined || review == undefined)
-        return {status: 403, body: {message: "username, club or review is empty"}};
-    var tmp = global.ClubList.get(club);
-    if (tmp == undefined || global.UsersList.get(username) == undefined)
-        return {status: 401, body: {message: "Bad request"}};
-    tmp.comments.push(review);
-    global.ClubList.set(club, tmp);
-    console.log(global.ClubList.get(club));
-    return {status: 200, body: tmp};
-}
 module.exports = router;
