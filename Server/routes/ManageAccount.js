0110deb1502ac6193092c68350674de6c18972bf
guillaume <guillaume.lemariey@epitech.eu>
2021-11-09 16:16:02 +0100

    [ADD] add postreview and postcomment

--- Server/routes/ManageAccount.js
+++ Server/routes/ManageAccount.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var users = new Map();
 var Response = require('../object/response.js').Response;
 
 router.post('/login', function (req, res, next)
 {
-    let response = login(req);
-    res.status(response.status).json(response.body);
+    try {
+        let response = login(req);
+        res.status(response.status).json(response.body);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal error"});
+    }
 });
 
 router.post('/register', function (req, res, next)
 {
-    let response = register(req);
-    res.status(response.status).json(response.body);
+    try {
+        let response = register(req);
+        res.status(response.status).json(response.body);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Internal error"});
+    }
 });
 
 function register(req)
@@ -21,12 +30,11 @@ function register(req)
     var password = req.body.password;
 
     if (username == undefined || password == undefined)
-        return new Response(403, "username or password is empty");
-    if (users.get(username) != undefined)
-        return new Response(401, "This user already exist");
-    else
-        users.set(username, password);
-    return new Response(200, "User " + username + " well created");
+        return new Response(403, {message: "username or password is empty"});
+    if (global.UsersList.get(username) != undefined)
+        return new Response(401, {message: "This user already exist"});
+    global.UsersList.set(username, password);
+    return new Response(200, {message: "User " + username + " well created"});
 }
 
 function login(req)
@@ -35,10 +43,10 @@ function login(req)
     var username = req.body.username;
     var password = req.body.password;
     if (username == undefined || password == undefined)
-        return new Response(403, "username or password is empty");
-    if (users.get(username) == password)
-        return new Response(200, "You are now connected");
+        return new Response(403, {message: "username or password is empty"});
+    if (global.UsersList.get(username) == password)
+        return new Response(200, {message : "You are now connected", username : username});
     else
-        return new Response(401, "The username and password doesn't match");
+        return new Response(401, {message: "The username and password doesn't match"});
 }
 module.exports = router;
---
+++ Server/routes/ManageReviews.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var Response = require('../object/response.js').Response;
+
+router.post('/PostComment', function (req, res, next)
+{
+    try {
+    let response = PostComment(req);
+    res.status(response.status).json(response.body);
+    } catch (error) {
+        return new Response(500, {message : "Internal error"});
+    }
+});
+
+router.post('/PostReview', function (req, res, next)
+{
+    try {
+    let response = PostReview(req);
+    res.status(response.status).json(response.body);
+    } catch (error) {
+        return new Response(500, {message : "Internal error"});
+    }
+});
+
+function PostReview(req)
+{
+    var username = req.body.username;
+    var club = req.body.club;
+    var review = req.body.review;
+
+    if (review == undefined || Number(review) == 'NaN' || username == undefined || club == undefined)
+        return new Response(403, {message: "username, club or review is undefined"});
+    var tmp = global.ClubList.get(club);
+    if (tmp == undefined || global.UsersList.get(username) == undefined)
+        return new Response(401, {message: "Bad request"});
+    var i = 0;
+    var rate = 0;
+    while (i < tmp.reviews) {
+        rate += tmp.rate;
+        i++;
+    }
+    tmp.reviews++;
+    tmp.rate = (rate + review) / tmp.reviews;
+    global.ClubList.set(club, tmp);
+    return new Response(200, tmp);
+}
+
+function PostComment(req)
+{
+    var username = req.body.username;
+    var club = req.body.club;
+    var review = req.body.review;
+
+    if (username == undefined || club == undefined || review == undefined)
+        return new Response(403, {message: "username, club or review is empty"});
+    var tmp = global.ClubList.get(club);
+    if (tmp == undefined || global.UsersList.get(username) == undefined)
+        return new Response(401, {message: "Bad request"});
+    tmp.comments.push(review);
+    global.ClubList.set(club, tmp);
+    console.log(global.ClubList.get(club));
+    return new Response(200, tmp);
+}
+module.exports = router;
