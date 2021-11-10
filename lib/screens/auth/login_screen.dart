dfee469b752496178558f323dafd70f0eb3d8835
tadam <mathis.vialle@epitech.eu>
2021-11-10 13:52:22 +0100

    fix(codeStyle): all files of the authentication widget are less than one hundred lines

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,9 +1,7 @@
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-import 'package:techno_clubs_berlin/API/api_manager.dart';
-import 'package:techno_clubs_berlin/constants/routes.dart';
+import 'package:techno_clubs_berlin/components/auth/auth_form/login_form.dart';
+import 'package:techno_clubs_berlin/components/auth/auth_navigation/login_navigation.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -15,31 +13,13 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final ApiManager _apiProvider = ApiManager(client: http.Client());
   final _formKey = GlobalKey<FormState>();
-
   bool _isLoading = false;
 
-  signIn() async {
-    if (_emailController.text.isEmpty || _passwordController.text.isEmpty) {
-      Get.snackbar('Error', 'Please verify your informations');
-      return;
-    }
-    await _apiProvider
-        .loginUser(_emailController.text, _passwordController.text)
-        .then((value) => {
-              Get.snackbar('Login Success', 'logged in as ' + value.name),
-              setState(() {
-                _isLoading = false;
-              }),
-              Get.offNamed(vueListClub),
-            })
-        .onError((error, stackTrace) => {
-              setState(() {
-                _isLoading = false;
-              }),
-              Get.snackbar('Login failed', error.toString()),
-            });
+  changeLoadingState(bool loadingState) {
+    setState(() {
+      _isLoading = loadingState;
+    });
   }
 
   @override
@@ -74,78 +54,21 @@ class _LoginScreenState extends State<LoginScreen> {
                             flex: 2,
                             child: Container(
                                 alignment: Alignment.center,
-                                child: Form(
-                                  key: _formKey,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: Column(
-                                      children: <Widget>[
-                                        TextFormField(
-                                          key: const Key('email'),
-                                          controller: _emailController,
-                                          decoration: const InputDecoration(
-                                              labelText: 'Username'),
-                                          validator: (value) {
-                                            if (value == "" || value == null) {
-                                              return "Username is required";
-                                            }
-                                          },
-                                        ),
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(top: 10.0),
-                                          child: TextFormField(
-                                            key: const Key('password'),
-                                            controller: _passwordController,
-                                            decoration: const InputDecoration(
-                                                labelText: 'Password'),
-                                            obscureText: true,
-                                            validator: (value) {
-                                              if (value == "" ||
-                                                  value == null) {
-                                                return "Password is required";
-                                              }
-                                            },
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )),
+                                child: LoginForm(
+                                    emailController: _emailController,
+                                    passwordController: _passwordController,
+                                    formKey: _formKey)),
                           ),
                           Expanded(
                             flex: 1,
                             child: Container(
-                              alignment: Alignment.center,
-                              child: Column(
-                                children: <Widget>[
-                                  ElevatedButton(
-                                    onPressed: () => Get.toNamed(vueListClub),
-                                    child: const Text('Bypass login'),
-                                  ),
-                                  ElevatedButton(
-                                    key: const Key('login'),
-                                    onPressed: () {
-                                      if (_formKey.currentState!.validate()) {
-                                        setState(() {
-                                          _isLoading = true;
-                                        });
-                                        signIn();
-                                      }
-                                    }, //signIn,
-                                    child: const Text('Login'),
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () =>
-                                        Get.offNamed(registerRoute),
-                                    child: const Text(
-                                        'No account ? Go to register'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+                                alignment: Alignment.center,
+                                child: LoginNavigation(
+                                    emailController: _emailController,
+                                    passwordController: _passwordController,
+                                    formKey: _formKey,
+                                    changeLoadingState: changeLoadingState)),
+                          )
                         ],
                       ),
                     ),
--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -1,12 +1,9 @@
-import 'dart:developer';
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
 
-import 'package:techno_clubs_berlin/API/api_manager.dart';
-import 'package:techno_clubs_berlin/constants/routes.dart';
+import 'package:techno_clubs_berlin/components/auth/auth_form/register_form.dart';
+import 'package:techno_clubs_berlin/components/auth/auth_navigation/register_navigation.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -19,34 +16,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final ApiManager _apiProvider = ApiManager(client: http.Client());
   final _formKey = GlobalKey<FormState>();
-
   bool _isLoading = false;
 
-  Future<void> register() async {
-    if (_usernameController.text.isEmpty ||
-        _emailController.text.isEmpty ||
-        _passwordController.text.isEmpty) {
-      Get.snackbar('Error', 'All information are mandatory');
-      return;
-    }
-    await _apiProvider
-        .registerUser(_usernameController.text, _passwordController.text)
-        .then((value) => {
-              Get.snackbar('Register Success', 'Register as : ' + value.name),
-              setState(() {
-                _isLoading = false;
-              }),
-              Get.offNamed(vueListClub),
-            })
-        .onError((error, stackTrace) => {
-              log(error.toString()),
-              setState(() {
-                _isLoading = false;
-              }),
-              Get.snackbar('Register failed', error.toString()),
-            });
+  changeLoadingState(bool loadingState) {
+    setState(() {
+      _isLoading = loadingState;
+    });
   }
 
   @override
@@ -80,88 +56,23 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           flex: 2,
                           child: Container(
                               alignment: Alignment.center,
-                              child: Form(
-                                key: _formKey,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 25),
-                                  child: Column(
-                                    children: <Widget>[
-                                      TextFormField(
-                                        key: const Key('usernameRegister'),
-                                        controller: _usernameController,
-                                        decoration: const InputDecoration(
-                                            labelText: 'Username'),
-                                        validator: (value) {
-                                          if (value == "" || value == null) {
-                                            return "Username is required";
-                                          }
-                                        },
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 10.0),
-                                        child: TextFormField(
-                                            key: const Key('emailRegister'),
-                                            controller: _emailController,
-                                            decoration: const InputDecoration(
-                                                labelText: 'Email'),
-                                            validator: (value) {
-                                              if (value == "" ||
-                                                  value == null) {
-                                                return "Email is required";
-                                              }
-                                            }),
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 10.0),
-                                        child: TextFormField(
-                                          key: const Key('passwordRegister'),
-                                          controller: _passwordController,
-                                          decoration: const InputDecoration(
-                                              labelText: 'Password'),
-                                          obscureText: true,
-                                          validator: (value) {
-                                            if (value == "" || value == null) {
-                                              return "Password is required";
-                                            }
-                                          },
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                              child: RegisterForm(
+                                emailController: _emailController,
+                                passwordController: _passwordController,
+                                usernameController: _usernameController,
+                                formKey: _formKey,
                               )),
                         ),
                         Expanded(
                           flex: 1,
                           child: Container(
-                            alignment: Alignment.center,
-                            child: Column(
-                              children: <Widget>[
-                                ElevatedButton(
-                                  key: const Key('register'),
-                                  onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                      setState(() {
-                                        _isLoading = true;
-                                      });
-                                      register();
-                                    }
-                                  }, //signIn,
-
-                                  child: const Text('Register'),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () => Get.offNamed(loginRoute),
-                                  child: const Text(
-                                    'Already have an account ? Go to login',
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                              alignment: Alignment.center,
+                              child: RegisterNavigation(
+                                  usernameController: _usernameController,
+                                  passwordController: _passwordController,
+                                  emailController: _emailController,
+                                  formKey: _formKey,
+                                  changeLoadingState: changeLoadingState)),
                         ),
                       ],
                     ),
