1416348d60663135ec4a453d2643935fd4126ff2
tadam <mathis.vialle@epitech.eu>
2021-11-13 16:36:56 +0100

    unitTests(add listClubUnitTest): test club list request

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,17 @@
 import 'Unit_tests/test_authentification_mock.dart';
+import 'Unit_tests/test_vue_club_list_mock.dart';
 import 'Widgets/test_authentication_widget.dart';
 import 'Widgets/test_vue_club_list_widget.dart';
 
 void main() {
+  /*** Units Testing ***/
 
-  /* Units Testing */
-  // Test Authentication Screen with Unit tests
+  // Test Authentication Requests with Unit tests
   authUnitTest();
+  // Test List Club Requests with Unit tests
+  listClubUnitTest();
 
-  /* Widgets Testing */
+  /*** Widgets Testing ***/
 
   // Test Authentication Widgets
   testAuthWidgets();
