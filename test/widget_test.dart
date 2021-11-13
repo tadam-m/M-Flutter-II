4d7ce78672ef6fdc787b34b49c8e51e1ad27f660
tadam <mathis.vialle@epitech.eu>
2021-11-13 12:47:54 +0100

    test(ListClubVue): Widget ListView of List Club screen test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,17 @@
 import 'Unit_tests/test_authentification_mock.dart';
 import 'Widgets/test_authentication_widget.dart';
+import 'Widgets/test_vue_club_list_widget.dart';
 
 void main() {
+
+  /* Units Testing */
   // Test Authentication Screen with Unit tests
   authUnitTest();
-  // Test Authentication Widgets with Widget tests
+
+  /* Widgets Testing */
+
+  // Test Authentication Widgets
   testAuthWidgets();
+  // Test List Club Widgets
+  testListClubWidget();
 }
