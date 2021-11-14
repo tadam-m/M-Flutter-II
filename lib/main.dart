fe0002471bca6862d86c30c9b8e48f835dfe9b96
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-11-14 18:31:15 +0100

    Added routes for errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_offline/flutter_offline.dart';
 
-import 'package:techno_clubs_berlin/screens/auth/register_screen.dart';
-import 'package:techno_clubs_berlin/themes/themes.dart';
-import 'package:techno_clubs_berlin/screens/splash_screen.dart';
+import 'package:techno_clubs_berlin/constants/routes.dart';
 import 'package:techno_clubs_berlin/screens/auth/login_screen.dart';
-import 'constants/routes.dart';
-import 'screens/vue_club_list_screen.dart';
+import 'package:techno_clubs_berlin/screens/auth/register_screen.dart';
+import 'package:techno_clubs_berlin/screens/connectivity/protect_connectivity.dart';
+import 'package:techno_clubs_berlin/screens/connectivity/server_down.dart';
+import 'package:techno_clubs_berlin/screens/splash_screen.dart';
+import 'package:techno_clubs_berlin/screens/vue_club_list_screen.dart';
+import 'package:techno_clubs_berlin/themes/themes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Routing());
 }
 
 class Routing extends StatelessWidget {
@@ -28,47 +29,21 @@ class Routing extends StatelessWidget {
         ),
         GetPage<LoginScreen>(
           name: loginRoute,
-          page: () => const LoginScreen(),
+          page: () => const ProtectConnectivity(child: LoginScreen()),
         ),
         GetPage<RegisterScreen>(
           name: registerRoute,
-          page: () => const RegisterScreen(),
+          page: () => const ProtectConnectivity(child: RegisterScreen()),
         ),
         GetPage<VueClubListScreen>(
           name: vueListClub,
-          page: () => const VueClubListScreen(),
+          page: () => const ProtectConnectivity(child: VueClubListScreen()),
+        ),
+        GetPage<ServerDown>(
+          name: serverDown,
+          page: () => const ServerDown(),
         ),
       ],
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) {
-          return Scaffold(
-            body: OfflineBuilder(
-              connectivityBuilder: (
-                context,
-                connectivity,
-                child,
-              ) {
-                if (connectivity == ConnectivityResult.none) {
-                  return const Text('Oops, \n\nNow we are Offline!');
-                } else {
-                  return child;
-                }
-              },
-              builder: (context) => const Routing(),
-            ),
-          );
-        },
-      ),
     );
   }
 }
