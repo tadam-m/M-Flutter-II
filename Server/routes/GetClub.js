64813ef1f60a6ac2d823b0260c1b050f36bf14d1
guillaume <guillaume.lemariey@epitech.eu>
2021-11-14 16:16:34 +0100

    [EDIT] DeleteReview added to the backend side, and uuid added for reviews

--- Server/routes/GetClub.js
+++ Server/routes/GetClub.js
@@ -22,23 +22,16 @@ router.get("/getClub", function (req, res, next) {
 });
 
 function getList(req) {
-  var username = req.body.username;
-
-  if (username == undefined)
-    return { status: 403, body: { message: "username is empty" } };
-  if (global.UsersList.get(username) == undefined)
-    return { status: 401, body: { message: "Bad request" } };
   return { status: 200, body: global.ClubList };
 }
 
 function getClub(req) {
-  var username = req.body.username;
   var club = req.body.club;
 
-  if (username == undefined || club == undefined)
-    return { status: 403, body: { message: "username or club is empty" } };
+  if (club == undefined)
+    return { status: 403, body: { message: "club is empty" } };
   var tmp = global.ClubList.get(club);
-  if (tmp == undefined || global.UsersList.get(username) == undefined)
+  if (tmp == undefined)
     return { status: 401, body: { message: "Bad request" } };
   return { status: 200, body: tmp };
 }
