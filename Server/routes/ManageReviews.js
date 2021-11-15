1583eb1ab551915177f3a455403cccd5c8ed3421
Ersikan <julien.philippon@epitech.eu>
2021-11-15 18:04:46 +0100

    style(Server): format

--- Server/routes/ManageReviews.js
+++ Server/routes/ManageReviews.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
-var uuid = require('uuid');
+var uuid = require("uuid");
 
-router.post("/PostReview", function (req, res, next)
-{
+router.post("/PostReview", function (req, res, next) {
   try {
     let response = PostReview(req);
     res.status(response.status).json(response.body);
@@ -12,17 +11,15 @@ router.post("/PostReview", function (req, res, next)
   }
 });
 
-router.post("/DeleteReview", function (req, res, next)
-{
-    try {
-        let response = DeleteReview(req);
-        res.status(response.status).json(response.body);
-      } catch (error) {
-        return { status: 500, body: { message: { message: "Internal error" } } };
-      }
+router.post("/DeleteReview", function (req, res, next) {
+  try {
+    let response = DeleteReview(req);
+    res.status(response.status).json(response.body);
+  } catch (error) {
+    return { status: 500, body: { message: { message: "Internal error" } } };
+  }
 });
-router.get("/GetReview", function (req, res, next)
-{
+router.get("/GetReview", function (req, res, next) {
   try {
     let response = GetReview(req);
     res.status(response.status).json(response.body);
@@ -44,35 +41,29 @@ function GetReview(req) {
   return { status: 200, body: tmp.reviews };
 }
 
-function DeleteReview(req)
-{
-    var uuid = req.body.uuid;
-    var club = req.body.club;
-    
-    if (club == undefined || uuid == undefined) 
-        return {
-            status: 403,
-            body: { message: { message: "username, club or review is undefined" } },
-      };
-    var tmp = global.ClubList.get(club);
-    if (tmp == undefined)
-        return { status: 401, body: { message: { message: "Bad request" } } };
-    let i = 0;
-    for (; tmp.reviews[i]; i++)
-    {
-        console.log(tmp.reviews[i]);
-        if (tmp.reviews[i].uuid == uuid)
-            break;
-    }
-    if (i >= tmp.reviews.lenght)
-        return { status: 401, body: { message: { message: "Bad request" } } };
-    global.ClubList.get(club).reviews.splice(i, 1);
-    return { status: 200, body: global.ClubList.get(club) };
-    
+function DeleteReview(req) {
+  var uuid = req.body.uuid;
+  var club = req.body.club;
 
+  if (club == undefined || uuid == undefined)
+    return {
+      status: 403,
+      body: { message: { message: "username, club or review is undefined" } },
+    };
+  var tmp = global.ClubList.get(club);
+  if (tmp == undefined)
+    return { status: 401, body: { message: { message: "Bad request" } } };
+  let i = 0;
+  for (; tmp.reviews[i]; i++) {
+    console.log(tmp.reviews[i]);
+    if (tmp.reviews[i].uuid == uuid) break;
+  }
+  if (i >= tmp.reviews.lenght)
+    return { status: 401, body: { message: { message: "Bad request" } } };
+  global.ClubList.get(club).reviews.splice(i, 1);
+  return { status: 200, body: global.ClubList.get(club) };
 }
-function PostReview(req)
-{
+function PostReview(req) {
   var username = req.body.reviewerName;
   var reviewContent = req.body.reviewContent;
   var mark = req.body.mark;
